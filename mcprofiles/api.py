564d4a0c698d6e039e6ab085b0397c38c0edf19e
Brad Reardon <brad.jay.reardon@gmail.com>
2014-05-06 22:07:01 -0400

    Added ability to convert UUID to username.

--- mcprofiles/api.py
+++ mcprofiles/api.py
@@ -1,6 +1,24 @@
 import requests
 from json import JSONEncoder
 
+from .exceptions import *
+
+def get_session(uuid):
+    """
+    Gets the session for a given UUID, containing all sorts of fun details.
+
+    If a UUID is invalid, an InvalidUsername exception will be raised.
+
+    :param uuid: The UUID to get session information for.
+    :return: Decoded JSON for the session of the given UUID.
+    """
+    r = requests.get("https://sessionserver.mojang.com/session/minecraft/profile/%s" % uuid)
+
+    if r.text == "":
+        raise InvalidUsername
+
+    return r.json()
+
 def to_uuid(users):
     """
     Provides easy method for checking UUID of one or multiple users.
@@ -23,3 +41,17 @@ def to_uuid(users):
         resp[pair['name']] = pair['id']
 
     return resp
+
+def to_user(uuid):
+    """
+    Provides method for checking username of a single UUID.
+    Will always return a string containing the username.
+
+    If a UUID is invalid, an InvalidUsername exception will be raised.
+
+    :param uuid: The UUID to look up through the API.
+    :return: The username associated with the given UUID.
+    """
+    s = get_session(uuid)
+
+    return s['name']
